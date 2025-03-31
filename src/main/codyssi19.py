cb47be2df61b0343d3583bff561631e063cf20cc
pareronia <49491686+pareronia@users.noreply.github.com>
2025-03-31 18:22:40 +0200

    Codyssi Problem 19 - cleanup

--- src/main/codyssi19.py
+++ src/main/codyssi19.py
@@ -4,7 +4,6 @@ from collections import defaultdict
 from codyssi.common import InputData
 from codyssi.common import SolutionBase
 from codyssi.common import codyssi_samples
-from codyssi.common import log
 from codyssi.common import to_blocks
 
 TEST = """\
@@ -34,42 +33,41 @@ Output3 = str
 
 
 class Node:
-    def __init__(self, val: int):
-        self.val: int = val
+    def __init__(self, val: tuple[int, str]):
+        self.val: tuple[int, str] = val
         self.left: Node | None = None
         self.right: Node | None = None
 
-    def __repr__(self) -> str:
-        return str(self.val)
-
-
-class Node2:
-    def __init__(self, val: tuple[int, str]):
-        self.val: tuple[int, str] = val
-        self.left: Node2 | None = None
-        self.right: Node2 | None = None
-
-    def __repr__(self) -> str:
-        return str(self.val)
-
 
 class Solution(SolutionBase[Output1, Output2, Output3]):
-    def part_1(self, input: InputData) -> Output1:
-        def insert(root: Node | None, val: int) -> Node | None:
+    def parse(
+        self, input: list[str]
+    ) -> tuple[list[tuple[int, str]], Node | None]:
+        def insert(root: Node | None, val: tuple[int, str]) -> Node | None:
             if root is None:
                 return Node(val)
-            if root.val == val:
+            if root.val[0] == val[0]:
                 return root
-            if root.val < val:
-                root.right = insert(root.right, val)
-            else:
+            if root.val[0] < val[0]:
                 root.left = insert(root.left, val)
+            else:
+                root.right = insert(root.right, val)
             return root
 
+        ids = list[tuple[int, str]]()
+        for line in input:
+            s, _, s_id = line.split()
+            ids.append((int(s_id), s))
+        root: Node | None = Node(ids[0])
+        for id in ids[1:]:
+            root = insert(root, id)
+        return ids, root
+
+    def part_1(self, input: InputData) -> Output1:
         def getLevel(root: Node | None, target: int, level: int) -> int:
             if root is None:
                 return -1
-            if root.val == target:
+            if root.val[0] == target:
                 return level
             leftLevel = getLevel(root.left, target, level + 1)
             if leftLevel != -1:
@@ -77,53 +75,24 @@ class Solution(SolutionBase[Output1, Output2, Output3]):
             return getLevel(root.right, target, level + 1)
 
         blocks = to_blocks(input)
-        ids = list[int]()
-        for line in blocks[0]:
-            _, _, s_id = line.split()
-            ids.append(int(s_id))
-        root: Node | None = Node(ids[0])
-        for i in range(1, len(ids)):
-            root = insert(root, ids[i])
+        ids, root = self.parse(blocks[0])
         by_level = defaultdict[int, set[int]](set)
         for id in ids:
-            lvl = getLevel(root, id, 1)
-            by_level[lvl].add(id)
-        max_sum = max(sum(v) for _, v in by_level.items())
-        return max_sum * len(by_level)
+            lvl = getLevel(root, id[0], 1)
+            by_level[lvl].add(id[0])
+        return len(by_level) * max(sum(v) for v in by_level.values())
 
     def part_2(self, input: InputData) -> Output2:
-        def insert(root: Node2 | None, val: tuple[int, str]) -> Node2 | None:
-            if root is None:
-                return Node2(val)
-            if root.val == val:
-                return root
-            if root.val < val:
-                root.right = insert(root.right, val)
-            else:
-                root.left = insert(root.left, val)
-            return root
-
         blocks = to_blocks(input)
-        ids = list[tuple[int, str]]()
-        for line in blocks[0]:
-            s, _, s_id = line.split()
-            ids.append((int(s_id), s))
-        root: Node2 | None = Node2(ids[0])
-        r = root
-        for i in range(1, len(ids)):
-            r = insert(r, ids[i])
-        assert root is not None
+        _, root = self.parse(blocks[0])
         ans = []
         while root:
             ans.append(root.val[1])
-            if root.val[0] < 500_000:
-                root = root.right
-            else:
-                root = root.left
+            root = root.left if root.val[0] < 500_000 else root.right
         return "-".join(ans)
 
     def part_3(self, input: InputData) -> Output3:
-        def LCA(root: Node2 | None, n1: Node2, n2: Node2) -> Node2 | None:
+        def LCA(root: Node | None, n1: Node, n2: Node) -> Node | None:
             if root is None:
                 return None
             if root.val[0] > n1.val[0] and root.val[0] > n2.val[0]:
@@ -132,18 +101,7 @@ class Solution(SolutionBase[Output1, Output2, Output3]):
                 return LCA(root.left, n1, n2)
             return root
 
-        def insert2(root: Node2 | None, val: tuple[int, str]) -> Node2 | None:
-            if root is None:
-                return Node2(val)
-            if root.val[0] == val[0]:
-                return root
-            if root.val[0] < val[0]:
-                root.left = insert2(root.left, val)
-            else:
-                root.right = insert2(root.right, val)
-            return root
-
-        def find(root: Node2 | None, val: int) -> Node2 | None:
+        def find(root: Node | None, val: int) -> Node | None:
             if root is None:
                 return None
             if root.val[0] == val:
@@ -153,25 +111,12 @@ class Solution(SolutionBase[Output1, Output2, Output3]):
             else:
                 return find(root.right, val)
 
-        log("hey")
         blocks = to_blocks(input)
-        ids = list[tuple[int, str]]()
-        for line in blocks[0]:
-            s, _, s_id = line.split()
-            ids.append((int(s_id), s))
-        root: Node2 | None = Node2(ids[0])
-        v1 = int(blocks[1][0].split()[2])
-        v2 = int(blocks[1][1].split()[2])
-        log((v1, v2))
-        for i in range(1, len(ids)):
-            root = insert2(root, ids[i])
-        assert root is not None
-        log(root)
-        r1 = find(root, v1)
-        r2 = find(root, v2)
+        _, root = self.parse(blocks[0])
+        r1 = find(root, int(blocks[1][0].split()[2]))
+        r2 = find(root, int(blocks[1][1].split()[2]))
         assert r1 is not None
         assert r2 is not None
-        log((r1.val[1], r2.val[1]))
         lca = LCA(root, r1, r2)
         assert lca is not None
         return lca.val[1]
